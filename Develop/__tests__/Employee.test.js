cff8cb96d5b621d9a0f96b611ef06d2705aea308
Mary Margaret <mmlawton15@gmail.com>
2022-01-11 10:30:19 -0500

    got the code to prompt the user for manager information and generate an html page, woohoo

--- Develop/__tests__/Employee.test.js
+++ Develop/__tests__/Employee.test.js
@@ -1,2 +1,8 @@
 //jest tests
 //must have the following properties and methods: name, id, email, getName(), getID(), getEmail(), getRole() this last one returns "Employee"
+
+const checkIfEqual = require('../lib/Employee.js');
+
+test('checks if 10 is equal to 10', () => {
+    expect(checkIfEqual(10, 10)).toBe(true);
+});
--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,13 @@
-//runs application
-// GIVEN a command-line application that accepts user input
+// DONE - GIVEN a command-line application that accepts user input
 // WHEN I am prompted for my team members and their information
 // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
 // WHEN I click on an email address in the HTML
 // THEN my default email program opens and populates the TO field of the email with the address
 // WHEN I click on the GitHub username
 // THEN that GitHub profile opens in a new tab
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
+// DONE - WHEN I start the application
+// DONE - THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+// DONE - WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
 // WHEN I select the engineer option
 // THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
@@ -18,3 +17,98 @@
 // THEN I exit the application, and the HTML is generated
 //should add data validation if i have time
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+var mgrName;
+var mgrID;
+var mgrEmail;
+var mgrOfficeNumber;
+
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+//FUNCTION TO PROMPT USER FOR INFORMATION
+const promptUserForInformation = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'Please enter your Managers first and last name',
+            validate: managerNameInput => {
+                if (managerNameInput) {
+                    mgrName = managerNameInput;
+                    return true;
+                } else {
+                    console.log("Please enter your managers first and last name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: "managerEmpID",
+            message: 'Please enter your managers Employee ID',
+            validate: managerEmpIDInput => {
+                if (managerEmpIDInput) {
+                    mgrID = managerEmpIDInput;
+                return true;
+            } else {
+                console.log("Please enter your managers EmpID!");
+                return false;
+            }
+            }
+        },
+        {
+            type: 'input',
+            name: "managerEmail",
+            message: 'Please enter your managers email address',
+            validate: managerEmailInput => {
+                if (managerEmailInput) {
+                    mgrEmail = managerEmailInput;
+                return true;
+            } else {
+                console.log("Please enter your managers email address!");
+                return false;
+            }
+            }
+        },
+        {
+            type: 'input',
+            name: "managerOfficeNumber",
+            message: 'Please enter your managers Office Number',
+            validate: managerOfficeNumberInput => {
+                if (managerOfficeNumberInput) {
+                    mgrOfficeNumber = managerOfficeNumberInput;
+                return true;
+            } else {
+                console.log("Please enter your managers Office Number!");
+                return false;
+                }
+            }
+        }
+    ])
+    .then((data) => writeToFile(data))
+}
+
+//CAPTURE AND RETURN THE USERS INPUT
+const printProfileData = profileDataArr => {
+    console.log("=======");
+    return`
+    ${mgrName}
+    ${mgrOfficeNumber}
+    ${mgrEmail}
+    ${mgrID}
+    `
+}
+
+
+//WRITE THE HTML FILE
+function writeToFile(data) {
+    var printToString = printProfileData(data);
+    fs.writeFileSync(path.join(process.cwd(), "index.html"), printToString);
+}
+
+
+//CALL TO HTML FUNCTIONS
+promptUserForInformation().then(printProfileData())
